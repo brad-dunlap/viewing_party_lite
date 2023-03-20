4892a7df5ade297d0c317c9060cb0373a14a5dd6
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-03-20 17:53:02 -0600

    Complete model and validations

--- spec/models/viewing_party_spec.rb
+++ spec/models/viewing_party_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe User, type: :model do
+RSpec.describe ViewingParty, type: :model do
   describe "relationships" do
     it { should have_many :user_viewing_parties }
     it { should have_many(:users).through(:user_viewing_parties) }
