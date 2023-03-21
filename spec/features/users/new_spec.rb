6ef1d417be9779749915f6aab1a7f02160bf0a73
Brad Dunlap <braddunlapdpt@gmail.com>
2023-03-21 14:11:02 -0600

    feat: user_registration page with testing

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -5,9 +5,20 @@ RSpec.describe 'Register New User' do
     describe 'When I visit the user registration path (/register)' do
       it 'I should see a form to register.' do
         visit register_path
-				save_and_open_page
+
 				expect(page).to have_field('Name')
 				expect(page).to have_field('Email')
+			end
+
+			it 'creates a new user' do
+				visit register_path
+
+				fill_in 'Name', with: 'Jimbob'
+				fill_in 'Email', with: 'Jimbob@bobjim.com'
+
+				click_on 'Register'
+
+				expect(current_path).to eq("/users/#{User.last.id}")
 			end
 		end
 	end
