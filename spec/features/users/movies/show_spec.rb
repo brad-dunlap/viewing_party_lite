4cdad51ef62a0dc26f47c85a13a76d7b02c4201b
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-03-22 19:37:06 -0600

    Add create viewing party button

--- spec/features/users/movies/show_spec.rb
+++ spec/features/users/movies/show_spec.rb
@@ -43,8 +43,12 @@ RSpec.describe 'Movie Show Page' do
         expect(page).to have_content("Overview: Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.")
       end
 
-      xit 'I see a button to create a viewing party for this movie' do
+      it 'I see a button to create a viewing party for this movie' do
+        expect(page).to have_button("Create Viewing Party")
+      end
 
+      xit 'Lists the first 10 cast members' do
+        expect(page).to have_content()
       end
     end
   end
