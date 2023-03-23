e94cf9c9ed15df3ed09b29c7ee52b45cadc8873b
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-03-23 13:58:03 -0600

    Add attr for poros movie spec

--- spec/poros/movie_spec.rb
+++ spec/poros/movie_spec.rb
@@ -3,23 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe Movie do
-  before do
-    top_movies = File.read('spec/fixtures/top_movies.json')
-    stub_request(:get, 'https://api.themoviedb.org/3/movie/top_rated?api_key=0ec9f3b92d1ab9c1631a6787b9aa3458')
-      .with(
-        headers: {
-          'Accept' => '*/*',
-          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'User-Agent' => 'Faraday v2.7.4'
-        }
-      )
-      .to_return(status: 200, body: top_movies, headers: {})
-  end
+  let(:movie) { described_class.new({ 
 
-  let(:movie) { described_class.new({ title: 'The Godfather' }) }
+      title: 'The Godfather', 
+      vote_average: 8.7,
+      runtime: 175,
+      genres: ['Drama', 'Crime'],
+      overview: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.'
+
+    }) }
 
   it 'exists' do
     expect(movie).to be_a(described_class)
     expect(movie.title).to eq('The Godfather')
+    expect(movie.vote_average).to eq(8.7)
+    expect(movie.runtime).to eq(175)
+    expect(movie.genres).to eq(['Drama', 'Crime'])
+    expect(movie.overview).to eq('The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.')
   end
 end
