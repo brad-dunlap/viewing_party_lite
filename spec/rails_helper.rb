19ebff100ae982c130e7e24ca9885df9156a8286
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-03-24 13:52:12 -0600

    Add simplecov filter and sad path for flash message

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,9 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  add_filter "app/services/movie_service.rb"
+end
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
