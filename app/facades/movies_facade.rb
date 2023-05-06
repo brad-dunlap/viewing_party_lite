02b25039dbc5d25cf407c54a8249d2b81b1ff236
Brad Dunlap <braddunlapdpt@gmail.com>
2023-05-06 13:08:48 -0400

    update testing suite

--- app/facades/movies_facade.rb
+++ app/facades/movies_facade.rb
@@ -1,26 +1,30 @@
 class MoviesFacade
-	def top_twenty
-		MovieService.new.top_twenty_movies.map do |movie|
-			Movie.new(movie)
+  def initialize(movie_service = MovieService.new)
+    @movie_service = movie_service
+  end
+
+  def top_twenty
+    @movie_service.top_twenty_movies.map do |movie|
+      Movie.new(movie)
     end
-	end
+  end
 
-	def search(query)
-		MovieService.new.search_results(query).map do |movie|
-			Movie.new(movie)
-		end
-	end
+  def search(query)
+    @movie_service.search_results(query).map do |movie|
+      Movie.new(movie)
+    end
+  end
 
-	def movie_details(id)
-		movie = MovieService.new.movie_details(id)
-		Movie.new(movie)
-	end
+  def movie_details(id)
+    movie = @movie_service.movie_details(id)
+    Movie.new(movie)
+  end
 
-	def cast_details(id)
-		MovieService.new.cast_details(id)
-	end
+  def cast_details(id)
+    @movie_service.cast_details(id)
+  end
 
-	def reviews(id)
-		MovieService.new.reviews(id)
-	end
+  def reviews(id)
+    @movie_service.reviews(id)
+  end
 end
